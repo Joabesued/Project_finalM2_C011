6f803d32e4897a1675694247ebad78ce5841c936
Joabe costa de almeida oliveira <85427303+Joabesued@users.noreply.github.com>
2022-04-03 19:20:18 -0300

    40%

--- index.js
+++ index.js
@@ -5,16 +5,46 @@ const path = require("path");
 const app = express();
 const port = 3001;
 
+const itens = [
+  {
+  nome: "camiseta da versace",
+  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
+  tamanho: "M",
+  categoria: "roupa",
+},
+{
+  nome: "camiseta da versace",
+  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
+  tamanho: "M",
+  categoria: "roupa",
+},
+{
+  nome: "camiseta da versace",
+  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
+  tamanho: "M",
+  categoria: "roupa",
+},
+]
+
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req, res) => {
-  res.render("../views/index");
+app.get("/index.ejs",(req, res) => {
+  res.render("../views/index.ejs");
 });
 
-app.get("/catalogos",(req, res) => {
-    res.render("../views/catalogo");
+app.get("/meus-itens.ejs",(req, res) => {
+    res.render("../views/itens.ejs", {itens});
   });
+
+app.get("/adicionar.ejs",(req, res) => {
+    res.render("../views/adicionar.ejs");
+  });
+
+app.post("/add", (req, res) => {
+
+})
 
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
