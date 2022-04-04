1bcfeb1341fc47546626cb1788a33bd0dcc22b2d
Joabe costa de almeida oliveira <85427303+Joabesued@users.noreply.github.com>
2022-04-04 01:42:16 -0300

    40%

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
 
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const app = express();
-const port = 3001;
-
-const itens = [
-  {
-  nome: "camiseta da versace",
-  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
-  tamanho: "M",
-  categoria: "roupa",
-},
-{
-  nome: "camiseta da versace",
-  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
-  tamanho: "M",
-  categoria: "roupa",
-},
-{
-  nome: "camiseta da versace",
-  imagem: "https://cdn-images.farfetch-contents.com/15/35/56/28/15355628_27695914_600.jpg",
-  tamanho: "M",
-  categoria: "roupa",
-},
-]
-
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
-app.get("/index.ejs",(req, res) => {
-  res.render("../views/index.ejs");
-});
-
-app.get("/meus-itens.ejs",(req, res) => {
-    res.render("../views/itens.ejs", {itens});
-  });
-
-app.get("/adicionar.ejs",(req, res) => {
-    res.render("../views/adicionar.ejs");
-  });
-
-app.post("/add", (req, res) => {
-
-})
-
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
    
