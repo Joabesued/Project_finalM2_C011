007d96d1bc1753984b0b12413183a63418a0ac72
Joabe costa de almeida oliveira <85427303+Joabesued@users.noreply.github.com>
2022-04-04 21:11:19 -0300

    90%

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -11,7 +11,8 @@ const principal = (req, res) => {
 const getAll = async (req, res) => {
     try {
        const itens = await Item.findAll();
-       res.render("itens", {itens});
+       res.render("itens", {itens, itensPut: null,
+        itensDel : null})
     } catch (err){
         res.status(500).send({err: err.message});
     }
@@ -39,11 +40,61 @@ const create = async (req, res) => {
     }
 };
 
+const getById = async (req, res) => {
+    try{
+        const method = req.params.method;
+        const itens = await Item.findAll();
+        const item = await Item.findByPk(req.params.id);
+
+        if(method == "put"){
+            res.render("itens",{
+                itens,
+                itensPut : item,
+                itensDel : null,
+            });
+        } else {
+            res.render("itens",{
+                itens,
+                itensPut : null,
+                itensDel : item,
+        });
+    }
+        
+    } catch (err) {
+        res.status(500).send({err: err.message});
+    }
+}
     
+const update = async (req, res) => {
+
+     try {
+
+         const item = req.body;
+         await Item.update(item, {where: { id: req.params.id} });
+         res.redirect("/meus-itens"); 
+
+     } catch (err) {
+        res.status(500).send({err: err.message});
+     }
+}
+
+const remove = async (req, res) => {
+
+    try {
+
+        await Item.destroy({where:{id: req.params.id}});
+        res.redirect("/meus-itens");
+    } catch (err) {
+       res.status(500).send({err: err.message});
+    }
+}
 
 module.exports = {
     getAll,
     adicionar,
     principal,
     create,
+    getById,
+    update,
+    remove,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,8 @@ routes.get("/",itemController.principal);
 routes.get("/meus-itens", itemController.getAll);
 routes.get("/adicionar", itemController.adicionar);
 routes.post("/create",itemController.create);
+routes.get("/getById/:id/:method", itemController.getById);
+routes.post("/update/:id", itemController.update);
+routes.get("/remove/:id", itemController.remove);
 
 module.exports = routes;
